c7ffa73ea608e7c01c11130950b8e32380430661
Just1nBon1n <152336193+Just1nBon1n@users.noreply.github.com>
2025-03-13 12:56:22 -0700

    s8c2 gabarits hero.php

--- front-page.php
+++ front-page.php
@@ -1,41 +1,7 @@
     <?php get_header(); ?>
-    <?php
-        $hero_adresse = get_theme_mod('hero_adresse', 'Default Title');
-        $hero_auteur = get_theme_mod('hero_auteur', 'Default Title'); 
-        $hero_background = get_theme_mod('hero_background', ''); 
-        $hero_cta_text = get_theme_mod('hero_cta_text', 'Default Title');
-        $hero_cta_link = get_theme_mod('hero_cta_link', '#');
-        $hero_texte_couleur = get_theme_mod('hero_texte_couleur', '#000000');
+    <?php 
+        get_template_part('gabarits/hero'); 
     ?>
-    <section class="hero" style="background-image: url(<?php echo $hero_background ?>);
-                                 color: <?php echo $hero_texte_couleur; ?>">
-        <div class="hero__contenu global">
-            <h1 class="hero__titre"><?php bloginfo('name'); ?></h1>
-            <p class="hero__description">
-                <?php bloginfo('description'); ?>
-            </p>
-            <p class="hero__courriel">
-                <?php echo($hero_adresse); ?>
-            </p>
-            <p class="hero__adresse">
-                5800 Sherbrooke-est - Montréal (Québec) H1X 2A2
-            </p>
-            <p class="hero__auteur">
-                Auteur : <?php echo $hero_auteur; ?>
-            </p>
-            <form action="<?php echo $hero_cta_link;?>" method="get">
-                <button type="submit" class="hero__cta">
-                    <?php echo $hero_cta_text; ?>
-                </button>
-            </form>
-            <div class="hero__icone">
-                <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="20" height="20">
-                <img src="https://s2.svgbox.net/social.svg?ic=snapchat&color=000000" width="20" height="20">
-            </div>
-        </div>
-    </section>
     <section class="populaire">
         <div class="global">
             <?php if (have_posts()) : while (have_posts()) : the_post(); 
