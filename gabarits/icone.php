e1cf0714df7f1e8415bd6252cfe6f8b5b995ec0b
Just1nBon1n <152336193+Just1nBon1n@users.noreply.github.com>
2025-03-18 10:43:01 -0700

    s9c2 ajout icone

--- gabarits/icone.php
+++ gabarits/icone.php
@@ -3,4 +3,4 @@
   <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
   <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="20" height="20">
   <img src="https://s2.svgbox.net/social.svg?ic=snapchat&color=000000" width="20" height="20">
-</div>
+</div> 
