8625ebb8000e90e709761c5aca12cb129cc1f1ff
Just1nBon1n <152336193+Just1nBon1n@users.noreply.github.com>
2025-03-18 10:14:53 -0700

    s9c2 corrction footer

--- footer.php
+++ footer.php
@@ -5,41 +5,55 @@
 ?>
 <footer>
     <div class="piedpage global">
-        <section class="piedpage__s1">
-            <div class="piedpage__s1__titre">
-                Liens sur le voyages
-            </div>
-            <div class="piedpage__s1__externe">
+        <section class="piedpage__haut">
+            <section class="piedpage__s1">
+                <div class="piedpage__s1 titre">
+                    Liens sur le voyages
+                </div>
+                <div class="piedpage__s1__externe">
+                    <?php wp_nav_menu(array(
+                        "menu" => "externe",
+                        "container" => "nav",
+                    )); ?>
+                </div>
+            </section>
+            <section class="piedpage__s2">
+                <div class="piedpage__s2 titre">
+                    Adresse et recherche
+                </div>
+                <div class="piedpage__s2__adresse">
+                        <div class="piedpage__s2__adresse__coord">
+                            <?php echo $footer_adresse; ?>
+                        </div>
+                        <div class="piedpage__s2__adresse__telephone">
+                            Tel: <?php echo($footer_telephone); ?>
+                        </div>
+                        <div class="piedpage__s2__adresse__recherche">
+                            <?php get_search_form();   ?>
+                        </div>
+                </div>
+            </section>
+            <section class="piedpage__s3">
+                <div class="piedpage__s3 titre">
+                    Missions du club
+                </div>
+                <div class="piedpage__s3__description">
+                    <!-- La mission de Mondo Voyage est de réunir des passionnés de découverte autour d’expériences de voyage authentiques. Le club favorise l’exploration, le partage et la création de liens, tout en promouvant un tourisme responsable et enrichissant. -->
+                    <?php echo($footer_mission); ?>
+                </div>
+            </section> 
+        </section>
+        <section class="piedpage__bas">
+            <section class="piedpage__icones">
+                <?php get_template_part('gabarits/icone'); ?>
+            </section>
+            <section class="piedpage__menu">
                 <?php wp_nav_menu(array(
-                    "menu" => "externe",
-                    "container" => "nav",
+                    'menu' => 'principal',
+                    'container' => 'nav',
+                    'container_class' => 'entete__menu'
                 )); ?>
-            </div>
-        </section>
-        <section class="piedpage__s2">
-            <div class="piedpage__s2__titre">
-                Adresse et recherche
-            </div>
-            <div class="piedpage__s2__adresse">
-                    <div class="piedpage__s2__adresse__coord">
-                        <?php echo $footer_adresse; ?>
-                    </div>
-                    <div class="piedpage__s2__adresse__coord">
-                        Tel: <?php echo($footer_telephone); ?>
-                    </div>
-                    <div class="piedpage__s2__adresse__recherche">
-                        <?php get_search_form();   ?>
-                    </div>
-            </div>
-        </section>
-        <section class="piedpage__s3">
-            <div class="piedpage__s3__titre">
-                Missions du club
-            </div>
-            <div class="piedpage__s3__description">
-                <!-- La mission de Mondo Voyage est de réunir des passionnés de découverte autour d’expériences de voyage authentiques. Le club favorise l’exploration, le partage et la création de liens, tout en promouvant un tourisme responsable et enrichissant. -->
-                <?php echo($footer_mission); ?>
-            </div>
+            </section>
         </section>
     </div>
 </footer>
