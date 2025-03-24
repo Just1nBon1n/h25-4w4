2325f6e3c9fb085d7a3c0a038386a8fa117c2184
Just1nBon1n <e2263742@cmaisonneuve.qc.ca>
2025-03-24 14:00:32 -0400

    s9c1 creation 404.php

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<section class="populaire">
+    <div class="global">
+        <h2>Erreur 404</h2>
+        <p>La page que vous cherchez n'existe pas.</p>
+    </div>
+</section>
+<?php get_footer(); ?>
--- single-post.php
+++ single-post.php
@@ -17,6 +17,5 @@
         </div>
     </section>
     <?php get_footer(); ?>
-   
 </body>
 </html>
