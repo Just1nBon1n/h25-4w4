c8890ac4005f6c369d9ccb22eddc7e70f1d01dbc
Just1nBon1n <e2263742@cmaisonneuve.qc.ca>
2025-03-24 15:31:55 -0400

    s9c1 avancement bouton acceuil

--- 404.php
+++ 404.php
@@ -5,6 +5,9 @@
   <section class="erreur_404">
     <h1>Erreur 404</h1>
     <h3>La page que vous cherchez n'existe pas.</h3>
+    <p class="bouton_acceuil">
+      <a href="<?php echo home_url(); ?>">Revenir à l'accueil</a> <!-- Bouton avec lien vers l'accueil -->
+    </p>
   </section>
   <p class="erreur_barre bas">//////////////////////////////////////////////////////////////////////////////////////////</p>
 </main>
