fce44ad1f99772422ccf4f80076dfe44e38f878f
Just1nBon1n <e2263742@cmaisonneuve.qc.ca>
2025-03-31 15:16:04 -0400

    s10c1 INTRA menu

--- 404.php
+++ 404.php
@@ -13,14 +13,19 @@
     <!-- Titre de la page 404 -->
     <?php echo $erreur_404_titre; ?>
   </h1>
-  <h3 class="erreur_404_texte">
+  <p class="erreur_404_texte">
     <!-- Texte de la page 404 -->
     <?php echo $erreur_404_texte; ?>
-  </h3>
+  </p>
   <p class="bouton_acceuil">
     <!-- Bouton pour revenir à l'accueil -->
     <a href="<?php echo home_url(); ?>">Revenir à l'accueil</a> <!-- Bouton avec lien vers l'accueil -->
   </p>
+  <?php wp_nav_menu(array(
+                    'menu' => 'erreur_404',
+                    'container' => 'nav',
+                    'container_class' => 'erreur_404_menu'
+  )); ?>
 </section>
 
 <?php get_footer(); ?>
